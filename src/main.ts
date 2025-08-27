5d38c82b36d6fb74f4c1e7d9d07a33cdfd3ea6ed
Joisson Miguel <joissonm.miguel@gmail.com>
2025-08-27 10:45:12 +0100

    fix: usar PORT do Render

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`🚀 Application is running on: http://localhost:3000`);
 }
 
